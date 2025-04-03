0aa5dbc11b10eb0b450f828e6bf99fe9f2a1bc73
DanielDubnicky <113416260+DanielDubnicky@users.noreply.github.com>
2025-04-03 13:11:26 +0200

    lokator

--- tests/test_locator.py
+++ tests/test_locator.py
@@ -1,5 +1,18 @@
-with playwright.sync_api import sync_playwright
+import os
+from playwright.sync_api import sync_playwright, expect
+import urllib.parse
 
-with sync_playwright() as p:
-    
+def test_page_title():
+    file_path = os.path.abspath("./src/index.html")
+    file_path = urllib.parse.unquote(file_path)
+    print(file_path)
+    with sync_playwright() as p:
+        browser = p.chromium.launch(headless=False)
+        page = browser.new_page()
+        page.goto(f"file:///{file_path}")
+        nadpis_1 = page.locator('h1').first
+        expect(nadpis_1).to_be_visible()
+        browser.close()
+
+
 
